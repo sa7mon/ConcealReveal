d4361246befbf7b69ab52c1b14c173d4c947c4c7
sa7mon <dan.sa7mon@gmail.com>
2014-09-03 16:02:07 -0500

    Add cookie functionality.

--- index.php
+++ index.php
@@ -4,23 +4,34 @@
 			Project Created on: 9/2/14
 	*/
 
-	function saveIP ($ip) {
-		setcookie("TestCookie", $ip);
-	}
-
 	//Refer to flowchart sketch for process design
 
 	//First, let's check to see if there's a cookie set for this user.
-		if (isset($_COOKIE['IP'])) {
-			//If there is, show what the saved IP is and offer to clear it to start over.
-			//echo "true";
-			$cookieSet = true;
-			$cookieIP = $_COOKIE['IP'];
-		} else {
-			//If there is no cookie set: Proceed as normal.
-			//echo "false";
-			$cookieSet = false;
-		}
+	if (isset($_COOKIE['IP'])) {
+		//If there is, show what the saved IP is and offer to clear it to start over.
+		$cookieSet = true;
+		$cookieIP = $_COOKIE['IP'];
+	} else {
+		//If there is no cookie set: Proceed as normal.
+		$cookieSet = false;
+	}
+
+	if (isset($_POST['iptosave'])) {
+		// User has gotten here after clicking "Save my IP"
+		// We need to take the ip passed to us and set a cookie with it
+
+		// Set a cookie
+		setCookie("IP",$_POST['iptosave']);
+		header('Location: '.$_SERVER['REQUEST_URI']);
+	}
+	if (isset($_POST['iptoforget'])) {
+		// User has gotten here after clicking "Forget my IP"
+		// They have a cookie saved that we need to destroy.
+
+		//Destroy the cookie
+		setcookie ("IP", "", time() - 3600);
+		header('Location: '.$_SERVER['REQUEST_URI']);
+	}
 ?>
 <html>
 	<head>
@@ -34,10 +45,17 @@
 		<div class="content">
 			<?php 
 				if ($cookieSet == true) {
-					echo "<p>Your saved IP is: <span class='savedip'>".$cookieIP."</span></p>";
+					// User has a saved IP already. Display it and offer to forget it.
+					echo "<form name='forgetip' action='index.php' method='POST'>";
+					echo "<p>Your saved IP is: <span class='ip'><input type='text' name='iptoforget' value=".$cookieIP." readonly></span></p>";
+					echo "<input type='submit' value='Forget my saved IP' /><br />";
+					echo "</form>";
 				} else {
-					echo "Your current IP is: <span class='ip'>".$_SERVER['REMOTE_ADDR']."</span><br />";
+					// User doesn't have a saved IP. Offer to save it.
+					echo "<form name='saveip' action='index.php' method='POST'>";
+					echo "Your current IP is: <span class='ip'><input type='text' name='iptosave' value=".$_SERVER['REMOTE_ADDR']." readonly></span><br />";
 					echo "<input type='submit' value='Save my IP locally' /><br />";
+					echo "</form>";
 				};
 			?>
 			<br />
@@ -51,6 +69,9 @@
 			</select>
 			<br />
 			<br />
+			<?php
+				//echo ($cookieSet ? "<input type='hidden' value=".$cookieIP." />": "<input type='hidden' value=".$_SERVER['REMOTE_ADDR']." />");
+			?>
 			<input type="submit" value="Start Monitoring" />
 		</div>
 		<div class="footer">
